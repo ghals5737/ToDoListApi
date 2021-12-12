a78544a2d4ffa185e6eec8a8755ef158dc62e48b
boring-km <kms0644804@naver.com>
2021-12-12 21:45:49 +0900

    [bugfix/ScheduleApi] 컴파일 오류 수정

--- back/src/main/kotlin/com/example/back/service/ScheduleService.kt
+++ back/src/main/kotlin/com/example/back/service/ScheduleService.kt
@@ -1,6 +1,5 @@
 package com.example.back.service
 
-import com.example.back.dto.Schedule
 import com.example.back.dto.ScheduleReq
 import com.example.back.dto.ScheduleRes
 import org.bson.types.ObjectId
@@ -8,7 +7,7 @@ import org.bson.types.ObjectId
 interface ScheduleService {
     fun createSchedule(scheduleReq: ScheduleReq): ScheduleRes
     fun findScheduleByUserId(userId:String):List<ScheduleRes>
-    fun updateScheduleById(schedule: ScheduleReq,id:ObjectId):ScheduleRes
+    fun updateScheduleById(scheduleReq: ScheduleReq,id:ObjectId):ScheduleRes
     fun findByUserIdAndTitleLike(userId:String,title:String): List<ScheduleRes>
-    fun deleteScheduleById(schedule: ScheduleReq,id:ObjectId):ScheduleRes
+    fun deleteScheduleById(scheduleReq: ScheduleReq,id:ObjectId):ScheduleRes
 }
